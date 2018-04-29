4609cdcf012fce52af0632860dd6552b75673178
Victor Pereira <victtor.vpb@gmail.com>
2018-04-29 15:48:58 -0300

    change name database test

--- config/settings_test.py
+++ config/settings_test.py
@@ -4,7 +4,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
     }
 }
 

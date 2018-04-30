8bf8a6c0449999bfc54a2f7b54ddc4362689cd4d
Victor Pereira <victtor.vpb@gmail.com>
2018-04-29 22:13:33 -0300

    rename class tests

--- apps/upload_data/tests.py
+++ apps/upload_data/tests.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class TestModels(TestCase):
+class TestsViews(TestCase):
 
     def test_page_upload_200(self):
         url = reverse('upload_data:upload')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
+

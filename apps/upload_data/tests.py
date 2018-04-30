77cde2f0694180376d80f0ced0e89b1fb60969ac
Victor Pereira <victtor.vpb@gmail.com>
2018-04-29 22:29:43 -0300

    Create test insert model Invoice

--- apps/upload_data/tests.py
+++ apps/upload_data/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Invoices
 
 
-class TestsViews(TestCase):
+class TestsInvoicesViews(TestCase):
 
     def test_page_upload_200(self):
         url = reverse('upload_data:upload')
@@ -10,3 +11,21 @@ class TestsViews(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+
+class TestsInvoicesModel(TestCase):
+
+    def setUp(self):
+        self.invoice_insert = Invoices.objects.create(
+            purchaser_name='João Silva',
+            item_description='R$10 off R$20 of food',
+            item_price='10.0',
+            purchase_count=2,
+            merchant_address='987 Fake St',
+            merchant_name="Bob's Pizza",
+        )
+
+    def test_insert(self):
+
+        invoice_select = Invoices.objects.get(pk=self.invoice_insert.id)
+
+        self.assertEqual(self.invoice_insert, invoice_select)
